22104f9aa37efd4952cdae8e8645adf5f46977a0
Avoler0 <jys1391@gmail.com>
2022-10-16 17:52:55 +0900

    Feat 어드민 페이지

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import styled from "styled-components"
 
 
 export default function Admin(){
+  const [account,setAccount] = useState({
+    id:null,
+    pw:null
+  });
+  
+  function idOnChange(event:any){
+    const id = event.target.value;
+    setAccount({...account , id:id})
+  }
+  function pwOnChange(event:any){
+    const pw = event.target.value;
+    setAccount({...account , pw:pw})
+  }
   return <Container>
     어드민 로그인
+    <input type="text" onChange={idOnChange} />
+    <input type="password" onChange={pwOnChange}/>
   </Container>
 }
 
